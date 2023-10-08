4d7e0764f7310baf9959bfe53f931a0ff963b14f
flick-ai <1904456330@qq.com>
2023-10-08 15:38:33 +0800

    add zone

--- game/game.py
+++ game/game.py
@@ -1,32 +1,56 @@
-
+from typing import List
+from player import GeniusPlayer
+import random
 
 class GeniusGame:
-    def __init__(self) -> None:
+    def __init__(self, seed, player0_deck, player1_deck) -> None:
         self.num_players = 3
+        self.seed = seed
+        random.seed(seed)
+        self.first_player: bool
+        self.active_player: bool
+        player0 = GeniusPlayer(player0_deck)
+        player1 = GeniusPlayer(player1_deck)
+        self.players = [player0, player1]
 
-    def init_game(self, player1, player2):
+    def init_game(self):
         '''
         初始化阶段,包括选择起始手牌,选择出战角色
         '''
-        # TODO: 
-        # 完成一个初始选择牌的代码
-        
+        # 决定谁方先手
+        first = random.choice([0, 1])
+        self.first_player = first
+
+        # 初始化双方状态
+        self.set_hand_card()
         self.set_active_character()
-        pass
 
     def step(self):
         '''
         回合轮次
         '''
-        pass
+        self.roll_phase()
+        self.action_phase()
+        self.end_phase()
+        
+    def set_hand_card(self):
+        for player in self.players:
+            pass
 
     def set_active_character(self):
         '''
         选择出战角色
         '''
         pass
-
-    def roll_phase(self):
+    
+    def roll_phase(self, player, now_dice):
         '''
         掷骰子阶段, 输入为需要投掷的骰子个数和每个的类别(默认为8和随机),返回一个数组
         '''
+        if now_dice is None:
+            # 此时为第一次投掷
+            return List
+        else: 
+            pass
+
+    
--- game/player.py
+++ game/player.py
@@ -1,21 +1,31 @@
-from card import *
+import card
+from zone import Card_Zone
+
 
 class GeniusPlayer:
     def __init__(self, deck) -> None:
         # 获取基本的牌组信息
-        self.id = deck.id
         self.character_card = deck.character
-        self.hand_card = deck.card
 
         # 环境中的基本状态
         self.dice_zone: None
         self.card_zone: None
         self.support_zone: None
         self.summons_zone: None
-        self.card_zone: None
-        self.hand_zone: None
+        self.card_zone = Card_Zone(deck.action_card) # 牌库区
+        self.hand_zone: None # 手牌区
         self.character_zone: None
         self.activate_satets: None
 
     def init_player(self):
         pass
+
+    def take_action(self, action):
+        '''
+        将action信号转化为环境信号
+        '''
+    
+    def generate_action_mask(self):
+        '''
+        生成action的mask
+        '''
