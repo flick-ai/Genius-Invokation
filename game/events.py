9227ce8fdc64c5b916198a12b9fd0959202869dd
Bluixe <1q20943033@gmail.com>
2023-10-10 10:42:05 +0800

    modify events

--- game/events.py
+++ game/events.py
@@ -1,10 +1,15 @@
 from card.character.base import Damage
 from game.game import GeniusGame
 
+
+class Events:
+    def __init__(self) -> None:
+        pass
+
 class EventNode:
     '''事件节点'''
-    def __init__(self, event, before=None, next=None):
-        self.event = event
+    def __init__(self, action, before=None, next=None):
+        self.event = action
         self.before = before
         self.next = next
     
@@ -13,6 +18,11 @@ class EventNode:
             self.before.next = self.next
         if self.next:
             self.next.before = self.before
+
+
+class EventListDict:
+    def __init__(self) -> None:
+        pass
 
 
 class EventList:
@@ -31,15 +41,26 @@ class EventList:
 '''
 事件种类
 '''
+# class OnDealDamageEvent(EventNode):
 
-class Event:
-    '''事件基类'''
-    def __init__(self) -> None:
-        pass
 
-class OnDealDamageEvent(Event):
-    def __init__(self):
-        pass
+# class Event:
+#     '''事件基类'''
+#     def __init__(self) -> None:
+#         pass
 
-    def on_call(self, game: GeniusGame, damage: Damage) -> None:
-        pass
+#     def add_to_event_list(self, game: GeniusGame) -> None:
+#         pass
+
+#     def remove_from_event_list(self, game: GeniusGame) -> None:
+#         pass
+    
+#     def on_call(self, game: GeniusGame) -> None:
+#         pass
+
+# class OnDealDamageEvent(Event):
+#     def __init__(self):
+#         pass
+
+#     def on_call(self, game: GeniusGame, damage: Damage) -> None:
+#         pass
