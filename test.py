101a87bd391de63853f0e0cd870f555406d9653e
Bluixe <1q20943033@gmail.com>
2023-10-12 22:29:22 +0800

    maybe some changes

--- test.py
+++ test.py
@@ -1,20 +1,28 @@
 import numpy as np
+from typing import TYPE_CHECKING
+# a = [1,2,3,4,5]
+# # np.random.shuffle(a)
+# a.insert(5,6)
+# print(a)
 
-a = [1,2,3,4,5]
-# np.random.shuffle(a)
-a.insert(5,6)
-print(a)
+# a = []
 
-a = []
-
-class A:
-    def __init__(self):
-        self.a = []
-        self.b = 2
+# class A:
+#     def __init__(self):
+#         self.a = []
+#         self.b = 2
     
-    def aaa(self):
-        self.a.append(A())
+#     def aaa(self):
+#         self.a.append(A())
 
-alist = A()
-alist.aaa()
-print(alist.a[0].aaa)
+# alist = A()
+# alist.aaa()
+# print(alist.a[0].aaa)
+if TYPE_CHECKING:
+    from test2 import B
+
+
+def testf(a: 'B'):
+    print(a.a)
+
+testf(1.44)
