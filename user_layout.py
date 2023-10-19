9880d1a48612226c3e994d2514dd9185834756cf
Flick <79459207+flick-ai@users.noreply.github.com>
2023-10-20 02:12:33 +0800

    finish rich

--- user_layout.py
+++ user_layout.py
@@ -15,39 +15,69 @@ if TYPE_CHECKING:
 def layout(game: 'GeniusGame'):
     layout = Layout()
     layout.split_column(
-    Layout(name="upper"),
-    Layout(name="lower")
+        Layout(name="Game", ratio=1),
+        Layout(name="player0", ratio=2),
+        Layout(name="player1",ratio=2)
     )
+    layout['Game'].update(get_game_info(game))
+    for i in range(2):
+        player = "player" + str(i)
+        layout[player].split_row(
+            Layout(name="card", ratio=1),
+            Layout(name="support", ratio=2),
+            Layout(name="character", ratio=5),
+            Layout(name="summon", ratio=2),
+            Layout(name="dice", ratio=1)
+        )
 
-    layout["lower"].split_row(
-        Layout(name="card"),
-        Layout(name="support"),
-        Layout(name="character"),
-        Layout(name="summon"),
-        Layout(name="dice")
-    )
-    layout["lower"]['card'].size = None
-    layout["lower"]['card'].ratio = 1
-    layout["lower"]['summon'].size = None
-    layout["lower"]['summon'].ratio = 2
-    layout["lower"]['character'].size = None
-    layout["lower"]['character'].ratio = 5
-    layout["lower"]['support'].size = None
-    layout["lower"]['support'].ratio = 2
-    layout["lower"]['dice'].size = None
-    layout["lower"]['dice'].ratio = 1
+        layout[player]['support'].split_column(Layout(name="12"),Layout(name="34"),)
+        layout[player]['support']['12'].split_row(get_support(game.players[i], 0),get_support(game.players[i], 1))
+        layout[player]['support']['34'].split_row(get_support(game.players[i], 2),get_support(game.players[i], 3))
+        layout[player]['summon'].split_column(Layout(name="12"),Layout(name="34"),)
 
-    layout['lower']['card'].update(get_card(game.players[1]))
+        layout[player]['summon']['12'].split_row(get_summon(game.players[i], 0),get_summon(game.players[i], 1))
+        layout[player]['summon']['34'].split_row(get_summon(game.players[i], 2),get_summon(game.players[i], 3))
 
-    layout["lower"]['dice'].update(get_dice(game.players[1]))
-    layout['lower']['support'].update(get_support(game.players[1]))
-    layout['lower']['summon'].update(get_summon(game.players[1]))
+        layout[player]['character'].split_row(get_character(game.players[i], 0),
+                                                 get_character(game.players[i], 1),
+                                                 get_character(game.players[i], 2))
+
+        layout[player]['card'].update(get_card(game.players[i]))
+        layout[player]['dice'].update(get_dice(game.players[i]))
     return layout
+def get_game_info(game: 'GeniusGame'):
+    sponsor_message = Table.grid()
+    sponsor_message.add_column(no_wrap=True)
+    sponsor_message.add_row(
+        f"Round:{game.round}, Round_Phase:{game.game_phase.value}, Active_player:{game.active_player_index}, First_player:{game.first_player}",
+        style='blue',
+    )
+    message_panel = Panel(
+        Align.center(
+            Group(" ",Align.center(sponsor_message)),
+        ),
+        title="Game Information",
+    )
+    return message_panel
+def get_character(player: 'GeniusPlayer', idx):
+    sponsor_message = Table.grid()
+    sponsor_message.add_column(no_wrap=True)
+    character_list = player.character_list
+    sponsor_message.add_row(
+        character_list[idx].name,
+        style='blue',
+    )
+    message_panel = Panel(
+        Align.center(
+            Group(" ",Align.center(sponsor_message)),
+        ),
+        title="Support"+str(idx),
+    )
+    return message_panel
 
 def get_card(player: 'GeniusPlayer'):
     sponsor_message = Table.grid()
     sponsor_message.add_column(no_wrap=True)
-    sponsor_message.add_column(style="blue", justify="right")
     if player.hand_zone.num() != 0:
         for card in player.hand_zone.card:
             sponsor_message.add_row(
@@ -58,70 +88,49 @@ def get_card(player: 'GeniusPlayer'):
         Align.center(
             Group(" ",Align.center(sponsor_message)),
         ),
-        title="SupportZone",
+        title="HandCard"+str(player.index),
     )
     return message_panel
 
-def get_summon(player: 'GeniusPlayer'):
+def get_summon(player: 'GeniusPlayer', idx):
     sponsor_message = Table.grid()
     sponsor_message.add_column(no_wrap=True)
-    sponsor_message.add_column(style="blue", justify="right")
-    if player.summon_zone.num() != 0:
-        for summon in player.summon_zone.space:
-            sponsor_message.add_row(
-                summon.name,
-                style='blue',
-            )
+    if player.summons_zone.num() > idx:
+        summon = player.summons_zone.space[idx]
+        sponsor_message.add_row(
+            summon.name,
+            style='blue',
+        )
     message_panel = Panel(
         Align.center(
             Group(" ",Align.center(sponsor_message)),
         ),
-        title="SupportZone",
+        title="Summon"+str(idx+1),
     )
     return message_panel
 
 
-def get_support(player: 'GeniusPlayer'):
+def get_support(player: 'GeniusPlayer', idx):
     sponsor_message = Table.grid()
     sponsor_message.add_column(no_wrap=True)
-    sponsor_message.add_column(style="blue", justify="right")
-    if player.support_zone.num() != 0:
-        for support in player.support_zone.space:
-            sponsor_message.add_row(
-                support.name,
-                style='blue',
-            )
-
-    message_panel = Panel(
-        Align.center(
-            Group(" ",Align.center(sponsor_message)),
-        ),
-        title="SupportZone",
-    )
-    return message_panel
-
-def get_summon(player: 'GeniusPlayer'):
-    sponsor_message = Table.grid()
-    sponsor_message.add_column(no_wrap=True)
-    sponsor_message.add_column(style="blue", justify="right")
-    if player.summon_zone.num() != 0:
-        for summon in player.summon_zone.space:
-            sponsor_message.add_row(
-                summon.name,
-                style='blue',
-            )
+    if player.support_zone.num() > idx:
+        support = player.support_zone.space[idx]
+        sponsor_message.add_row(
+            support.name,
+            style='blue',
+        )
 
     message_panel = Panel(
         Align.center(
             Group(" ",Align.center(sponsor_message)),
         ),
-        title="SupportZone",
+        title="Support"+str(idx+1),
     )
     return message_panel
 
 def get_dice(player: 'GeniusPlayer'):
     sponsor_message = Table.grid()
-    sponsor_message.add_column(style="blue", justify="right")
+    sponsor_message.add_column(no_wrap=True)
     if player.dice_zone.show() is not None:
         for dice in player.dice_zone.show():
             sponsor_message.add_row(
@@ -134,6 +143,6 @@ def get_dice(player: 'GeniusPlayer'):
             Group(" ",Align.center(sponsor_message)),
             vertical="middle",
         ),
-        title="DiceZone",
+        title="DiceZone"+str(player.index),
     )
     return message_panel
