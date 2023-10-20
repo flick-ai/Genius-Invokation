9d5f38eb7a2dc15d500ad40a7565ed1a444df530
Dong Wentao <1134136579@qq.com>
2023-10-20 15:31:08 +0800

    add Itto!

--- genius_invocation/entity/entity.py
+++ genius_invocation/entity/entity.py
@@ -28,6 +28,8 @@ class Entity:
     def on_destroy(self, game):
         for action in self.registered_events:
             action.remove()
-
+    def update(self):
+        pass
+    
     def show(self):
         return self.name
