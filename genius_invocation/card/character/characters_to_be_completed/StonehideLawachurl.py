60a91833385dd05bee4b46ccee72f9ec80b83c23
xxxq <jiezichenku@gmail.com>
2023-10-23 23:46:20 +0800

    自动生成角色模板

---
+++ genius_invocation/card/character/characters_to_be_completed/StonehideLawachurl.py
@@ -0,0 +1,19 @@
+from genius_invocation.card.character.characters.import_head import *
+
+
+class StonehideLawachurl(Character):
+    id: int = 2601
+    name: str = "Stonehide Lawachurl"
+    name_ch = "丘丘岩盔王"
+    element: ElementType = ElementType.GEO
+    weapon_type: WeaponType = WeaponType.OTHER
+    country: CountryType = CountryType.HILICHURL
+    init_health_point: int = 8
+    max_health_point: int = 8
+    skill_list: List = []
+    max_power: int = 2
+
+    def __init__(self, game: 'GeniusGame', zone: 'CharacterZone', from_player: 'GeniusPlayer', index: int, from_character=None, talent=False):
+        super().__init__(game, zone, from_player, index, from_character)
+        self.power = 0
+        self.talent = talent
