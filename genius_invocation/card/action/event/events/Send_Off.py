1fcbc7fda43ec1a0946939da6599dac609eea18c
Dong Wentao <1134136579@qq.com>
2023-10-23 22:45:38 +0800

    add send off

--- genius_invocation/card/action/event/events/Send_Off.py
+++ genius_invocation/card/action/event/events/Send_Off.py
@@ -3,11 +3,11 @@ from genius_invocation.utils import *
 if TYPE_CHECKING:
     from genius_invocation.game.game import GeniusGame
 
-class Strategize(ActionCard):
-    id: int = 332004
-    name: str = 'Strategize'
-    name_ch = '运筹帷幄'
-    cost_num = 1
+class Send_Off(ActionCard):
+    id: int = 332013
+    name: str = 'Send Off'
+    name_ch = '送你一程'
+    cost_num = 2
     cost_type = CostType.WHITE
     card_type = ActionCardType.EVENT
 
@@ -15,4 +15,5 @@ class Strategize(ActionCard):
         super().__init__()
 
     def on_played(self, game: 'GeniusGame'):
-        game.active_player.get_card(num=2)
+        get_opponent(game).summon_zone.space[game.current_action.target_idx].minus_usage(game ,2)
+
