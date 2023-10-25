68f0eb9b8688745aa69608607fb01fd480d9e562
flick-ai <1904456330@qq.com>
2023-10-26 01:12:06 +0800

    fix chongyun

--- genius_invocation/card/character/characters/Chongyun.py
+++ genius_invocation/card/character/characters/Chongyun.py
@@ -1,7 +1,4 @@
 from genius_invocation.card.character.import_head import *
-from genius_invocation.event.Elemental_Reaction import GeniusGame
-from genius_invocation.game.game import GeniusGame
-from genius_invocation.utils import GeniusGame
 
 class Demonbane(NormalAttack):
     id: int = 11041
--- genius_invocation/card/character/characters_to_be_completed/Diluc.py
+++ genius_invocation/card/character/characters_to_be_completed/Diluc.py
@@ -1,5 +1,23 @@
 from genius_invocation.card.character.import_head import *
 
+class TemperedSword(NormalAttack):
+    id: int = 13011
+    name = "Tempered Sword"
+    name_ch = "淬炼之剑"
+    type: SkillType = SkillType.NORMAL_ATTACK
+    damage_type: SkillType = SkillType.NORMAL_ATTACK
+    main_damage_element: ElementType = ElementType.PHYSICAL
+    main_damage: int = 2
+    piercing_damage: int = 0
+    cost = [{'cost_num':1, 'cost_type':CostType.PYRO}, {'cost_num':2, "cost_type":CostType.BLACK}]
+    energy_cost: int = 0
+    energy_gain: int = 1
+
+    def on_call(self, game: 'GeniusGame'):
+        super().on_call(game)
+        self.resolve_damage(game)
+        self.gain_energy(game)
+        game.manager.invoke(EventType.AFTER_USE_SKILL, game)
 
 class Diluc(Character):
     id: int = 1301
