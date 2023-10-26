e918d0788e50361aadaa8942f104080d4408f23d
Flick <79459207+flick-ai@users.noreply.github.com>
2023-10-26 15:46:25 +0800

    add mirromaiden

--- genius_invocation/card/action/equipment/talent/talents/ProliferatingSpores.py
+++ genius_invocation/card/action/equipment/talent/talents/ProliferatingSpores.py
@@ -1,7 +1,6 @@
 from genius_invocation.card.action.equipment.talent.import_head import *
 from genius_invocation.card.character.characters.Jadeplume_Terrorshroom import *
 
-
 class ProliferatingSpores(TalentCard):
     id: int = 227011
     name: str = "Proliferating Spores"
@@ -10,6 +9,5 @@ class ProliferatingSpores(TalentCard):
     cost = [{'cost_num': 3, 'cost_type': 6}]
     cost_power = 0
     character = Jadeplume_Terrorshroom
-    skill_idx: int = 1
     def __init__(self) -> None:
         super().__init__()
