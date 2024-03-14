71a15d0a11180be155414bae29b0d2851541ba42
Dong Wentao <1134136579@qq.com>
2024-03-14 17:31:38 +0800

    fix bugs

--- genius_invocation/card/action/equipment/talent/talents/aColdBladeLikeaShadow.py
+++ genius_invocation/card/action/equipment/talent/talents/aColdBladeLikeaShadow.py
@@ -1,9 +1,9 @@
 from genius_invocation.card.action.equipment.talent.import_head import *
 from genius_invocation.card.character.characters.Lynette import * 
 
-class A_Cold_Blade_Like_a_Shadow(TalentCard):
+class aColdBladeLikeaShadow(TalentCard):
     id: int = 215081
-    name: str = "Keen Sight"
+    name: str = "A Cold Blade Like a Shadow"
     name_ch = "如影流露的冷刃"
     is_action = True
     cost = [{'cost_num': 3, 'cost_type': 4}]
