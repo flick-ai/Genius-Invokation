748aef44fab2752987423ce6ec106350839f1998
Bluixe <1q20943033@gmail.com>
2024-06-24 10:27:38 +0800

    some env codes

--- genius_invocation/rl/env/genius_env.py
+++ genius_invocation/rl/env/genius_env.py
@@ -1,8 +1,11 @@
 import gymnasium as gym
 from genius_invocation.game.game import GeniusGame
 from genius_invocation.main import code_to_deck
+from genius_invocation.game.action import Action
+from genius_invocation.utils import *
 
-class GeniusInvocationEnv(gym.Env):
+
+class GeniusInvocationEnv:
     '''
         Environment for Genius Invocation with the standard gym API.
     '''
@@ -22,14 +25,17 @@ class GeniusInvocationEnv(gym.Env):
 
     def reset(self):
         self.base_env = GeniusGame(self.deck[0], self.deck[1], self.seed)
+        state = self.encode_state()
+        return state
 
     def encode_state(self):
         # encode deck
-
+        
         # encode support
 
         # encode dice
-        
-
-
+        pass
+    def step(self, action):
+        processed_action = Action(*action)
+        self.base_env.step(processed_action)
 
